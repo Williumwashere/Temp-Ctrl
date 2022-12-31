589d6a88e2d534a2d8229f4e4042fa2f93de5e8a
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2022-12-30 20:47:39 -0600

    Update App.tsx

--- temp-control/src/App.tsx
+++ temp-control/src/App.tsx
@@ -2,15 +2,27 @@ import React, { useState } from "react";
 
 const App = () => {
   const [temperature, setTemperature] = useState(10);
+  const [color, setColor] = useState("cold");
+
+  const increaseTemperature = () => {
+    const newTemp = temperature + 1
+    setTemperature(newTemp);
+  };
+
+  const decreaseTemperature = () => {
+    const newTemp = temperature - 1
+    setTemperature(newTemp)
+  }
 
   return (
     <div className="app-container">
       <div className="temperature-display-container">
-        <div className="temperature-display">{temperature}°C</div>
+        <div className={`temperature-display ${color}`}>{temperature}°C</div>
       </div>
       <div className="button-container">
-        <button>+</button>
-        <button>-</button>
+        <button onClick={() => decreaseTemperature()}>-</button>
+        {}
+        <button onClick={() => increaseTemperature()}>+</button>
       </div>
     </div>
   );
